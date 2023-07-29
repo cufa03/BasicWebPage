e6b10a43a2d2c682be7bfc832f2ba45f5cb61623
Facundo Nadaf <facundo.nadaf@integrify.io>
2023-07-30 00:56:31 +0200

    Project created

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <div className="text-white">
+      <h1 className="w-full text-3xl font-bold text-[#00DF9A]">REACT.</h1>
+    </div>
+  );
+};
+
+export default NavBar;
