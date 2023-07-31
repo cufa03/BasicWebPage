56bade48780e1a6f1a095096fc62bbb4b815f942
Facundo Nadaf <facundo.nadaf@integrify.io>
2023-08-01 00:09:27 +0200

    Finished navbar and hero components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Analytics from './components/Analytics';
+import Hero from './components/Hero';
 import NavBar from './components/NavBar';
 function App() {
   return (
     <div>
       <NavBar />
+      <Hero />
+      <Analytics />
     </div>
   );
 }
